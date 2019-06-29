ab625a43d12d7c6a333036875de1a52a2bdfe4e9
Ahmed Ibrahim <ahmedie20022011@gmail.com>
2019-06-30 00:59:54 +0200

    Added EOI18 Qualification Problems

---
+++ EOI_Quals'18/B/main.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pi;
+typedef long long ll;
+pi ns[100000+9];
+int T, n, A, B;
+ 
+bool cmpr(pi p2, pi p1){
+    return (p2.first-p2.second)>( p1.first-p1.second);
+}
+ 
+int main(){
+    scanf("%d",&T);
+    while(T--){
+        scanf("%d %d %d", &n, &A, &B);
+        for(int i=0;i<n;i++) scanf("%d %d", &ns[i].first, &ns[i].second);
+        sort(ns, ns+n, cmpr);
+        int idx=n-1;
+        ll sum=0;
+        int re=B;
+        while(re--){
+            sum += ns[idx--].second;
+        }
+ 
+        idx=0;
+        while(A--){
+            sum += ns[idx++].first;
+        }
+ 
+        for(int i=idx;i<n-B;i++){
+            sum += max(ns[i].first, ns[i].second);
+        }
+ 
+        printf("%lld\n", sum);
+ 
+    }
+ 
+ 
+    return 0;
+}
