76953f9f448901768e17af122bd1f37d9d1014bb
ahmedibrahim404 <ahmedie20022011@gmail.com>
2021-09-10 19:01:00 +0200

    Added new topics to Senior Training

---
+++ Senior Training/MaximumBipartiteMatching/main.cpp
@@ -0,0 +1,25 @@
+int main() {
+    // ... reading the graph ...
+
+    mt.assign(k, -1);
+    vector<bool> used1(n, false);
+    for (int v = 0; v < n; ++v) {
+        for (int to : g[v]) {
+            if (mt[to] == -1) {
+                mt[to] = v;
+                used1[v] = true;
+                break;
+            }
+        }
+    }
+    for (int v = 0; v < n; ++v) {
+        if (used1[v])
+            continue;
+        used.assign(n, false);
+        try_kuhn(v);
+    }
+
+    for (int i = 0; i < k; ++i)
+        if (mt[i] != -1)
+            printf("%d %d\n", mt[i] + 1, i + 1);
+}
